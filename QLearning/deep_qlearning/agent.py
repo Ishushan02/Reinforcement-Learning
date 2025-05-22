3ebbcbdb2cce1dbef418e898559ef251e226fbc1
Ishushan02 <ishananand2002@gmail.com>
2025-05-22 07:48:53 -0700

    Epsilon Greedy Implementation

--- QLearning/deep_qlearning/agent.py
+++ QLearning/deep_qlearning/agent.py
@@ -2,12 +2,26 @@ import flappy_bird_gymnasium
 import gymnasium
 from deep_qlearning.dqn import DQN
 import torch
+from deep_qlearning.experience_replay import ReplayMemory
+import yaml
+import random
 
 DEVICE = torch.device("mps")
 
 class Agent:
 
-    def run(self, is_training = True, render = False):
+    def __init__(self, hyperparameter_set):
+        with open('./hyperparameters.yml', 'r') as file:
+            all_hyperparameter = yaml.safe_load(file)
+            hyperparameters = all_hyperparameter[hyperparameter_set]
+        
+        self.replay_memory = hyperparameters["replay_memory_size"]
+        self.epsilon_init  = hyperparameters["epsilon_init"]
+        self.epsilon_decay  = hyperparameters["epsilon_decay"]
+        self.epsilon_min   = hyperparameters["epsilon_min"]
+
+
+    def run(self, episodes=10000, is_training = True, render = False):
         # env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)CartPole-v1
         env = gymnasium.make("CartPole-v1", render_mode="human" if render else False)
 
@@ -16,24 +30,48 @@ class Agent:
         output_action = env.action_space.n
 
         policy_network = DQN(stateDimension=input_state, hiddenDimension=256, outActions=output_action).to(device=DEVICE)
+        rewards_per_episode = []
+        epsilon_history = []
+        if is_training:
+            memory = ReplayMemory(self.replay_memory)
+            epsilon = self.epsilon_init
 
-        obs, _ = env.reset()
         
-        while True:
-            # Next action:
-            # (feed the observation to your agent here)
-            action = env.action_space.sample()
+        
+        for each_episode in range(episodes):
+            state, _ = env.reset()
+            episode_reward = 0.0
 
-            # Processing:
-            obs, reward, terminated, _, info = env.step(action)
-            print(obs)
-            print(reward)
-            print(info)
-            print(terminated)
-            break
+            isTerminated = False
             
-            # Checking if the player is still alive
-            if terminated:
-                break
 
-        env.close()
+            while not isTerminated:
+                # Next action:
+                # (feed the observation to your agent here)
+
+                if is_training and random.random() < epsilon:
+                    action = env.action_space.sample()
+                else:
+                    action = policy_network(state).argmax()
+
+                new_state, reward, terminated, _, info = env.step(action)
+                # print(obs)
+                # print(reward)
+                # print(info)
+                # print(terminated)
+                # break
+                if is_training:
+                    memory.append([state, action, new_state, reward, terminated])
+
+                episode_reward += reward
+                state = new_state
+                
+                # Checking if the player is still alive
+                isTerminated = terminated
+                epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
+                epsilon_history.append(epsilon)
+            
+            env.close()
+            rewards_per_episode.append(episode_reward)
+
+            
